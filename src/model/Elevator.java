6209becd89fb52ac214bad4bff1aa87455c1fb99
Daniel Silva <danny@dannyps.net>
2018-04-27 16:52:40 +0100

    Add Elevator sutff and tests
    100% mutation coverage, wow :3

--- src/model/Elevator.java
+++ src/model/Elevator.java
@@ -1,5 +1,71 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Elevator {
 
+	List<NPC> npcs = new ArrayList<NPC>();
+	
+	private int capacity;
+	
+	public int getCapacity() {
+		return capacity;
+	}
+
+	public void setCapacity(int cap) {
+		if(cap<1)
+			throw new IllegalArgumentException("capacity must be 1 or more.");
+		this.capacity = cap;
+	}
+
+	public int getSpeed() {
+		return speed;
+	}
+
+	public void setSpeed(int speed) {
+		if(speed <= 0 || speed >5)
+			throw new IllegalArgumentException("speed must be between 1 and 5.");
+		this.speed=speed;
+	}
+
+	public List<NPC> getNpcs() {
+		return npcs;
+	}
+
+	private int speed;
+	
+	public Elevator(int cap, int speed) {
+		this.setCapacity(cap);
+		this.setSpeed(speed);
+	}
+	
+	public int getFillCount() {
+		return npcs.size();
+	}
+	
+	public int getEmptyCount(){
+		return this.capacity-this.getFillCount();
+	}
+	
+	public boolean addNPC(NPC npc){
+		if(this.getEmptyCount()<1 || this.npcExists(npc)) {
+			return false;
+		}else {
+			npcs.add(npc);
+			return true;
+		}
+	}
+	
+	private boolean npcExists(NPC npc) {
+		for(NPC n : this.npcs) {
+			if(n.equals(npc))
+				return true;
+		}
+		return false;
+	}
+
+	public boolean removeNPC(NPC npc) {
+		return npcs.remove(npc);
+	}
 }
---
+++ src/test/ElevatorTest.java
@@ -0,0 +1,63 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import model.Elevator;
+import model.NPC;
+
+public class ElevatorTest {
+
+	@Test(expected = IllegalArgumentException.class) 
+	public void badSpeed_1() { 
+		new Elevator(3, 6); 
+	}
+	
+	@Test(expected = IllegalArgumentException.class) 
+	public void badSpeed_2() { 
+		new Elevator(6, 0); 
+	}
+	
+	@Test(expected = IllegalArgumentException.class) 
+	public void badCapacity_1() { 
+		new Elevator(0, 0); 
+	}
+	
+	@Test(expected = IllegalArgumentException.class) 
+	public void badCapacity_2() { 
+		new Elevator(-99, 8); 
+	}
+	
+	@Test
+	public void GeneralTest() {
+		Elevator e = new Elevator(4,5);
+		assertEquals(5, e.getSpeed());
+		assertEquals(4, e.getCapacity());
+		
+		e.setCapacity(80);
+		assertEquals(80, e.getCapacity());
+		
+		NPC n1 = new NPC();
+		NPC n2 = new NPC();
+		assertTrue(e.addNPC(n1));
+		assertEquals(1, e.getFillCount());
+		assertEquals(79, e.getEmptyCount());
+		assertFalse(e.addNPC(n1));
+		assertFalse(e.removeNPC(n2));
+		assertTrue(e.addNPC(n2));
+		assertTrue(e.removeNPC(n2));
+		e.setCapacity(1);
+		assertFalse(e.addNPC(n2));
+		assertTrue(e.removeNPC(n1));
+		assertTrue(e.addNPC(n2));
+		for(NPC n : e.getNpcs()) {
+			assertEquals(n, n2);
+		}
+		
+		e.setSpeed(5);
+		assertEquals(5, e.getSpeed());
+		
+	}
+
+}
