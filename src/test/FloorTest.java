def9fccaf8ddd5f6d96fcd0c3418644909223220
Daniel Silva <danny@dannyps.net>
2018-04-28 16:46:34 +0100

    First major refactoring carried out successfully!

--- src/test/FloorTest.java
+++ src/test/FloorTest.java
@@ -4,7 +4,6 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-import model.Building;
 import model.Floor;
 
 public class FloorTest {
@@ -12,13 +11,13 @@ public class FloorTest {
 	@Test
 	public void generalTest() {
 		Floor f = new Floor(3);
-		f.setCapacity(1);
-		assertEquals(1, f.getCapacity());		
+		f.setCapacity(2);
+		assertEquals(2, f.getCapacity());		
 	}
 	
 	@Test(expected = IllegalArgumentException.class)
 	public void badCapacity() {
-		new Floor(0);
+		new Floor(1);
 	}
 	
 
