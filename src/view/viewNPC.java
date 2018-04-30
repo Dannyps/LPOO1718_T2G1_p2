6fc74abe9b3f44fa2186a937ea81237027a5a298
Fábio Gaspar <fabiodanielreis@hotmail.com>
2018-04-30 22:47:36 +0100

    Improved viewNPC

--- src/view/viewNPC.java
+++ src/view/viewNPC.java
@@ -3,7 +3,6 @@ package view;
 import javax.imageio.ImageIO;
 import javax.swing.JPanel;
 
-
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Panel;
@@ -22,46 +21,60 @@ import javax.swing.BoxLayout;
 import javax.swing.ImageIcon;
 
 import java.awt.Label;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
 
 public class viewNPC extends JPanel {
-	
+
 	private String imgPath;
 	private int floorDest;
-	
+
 	/**
 	 * Create the panel.
 	 */
 	public viewNPC(String imgPath, int floorDest) {
 		// set current image
 		this.imgPath = imgPath;
-		
+
 		// set destination floor
 		this.floorDest = floorDest;
-		
+
 		// transparency
-		this.setOpaque(true);
+		this.setOpaque(false);
 		
-		// layout grid
-		setLayout(new GridLayout(2, 1, 0, 0));
-		
-		// label for floor destination
-		JLabel lblDestFloor = new JLabel(Integer.toString(floorDest));
-		lblDestFloor.setFont(new Font("Tahoma", Font.BOLD, 14));
-		lblDestFloor.setHorizontalAlignment(SwingConstants.RIGHT);
-		lblDestFloor.setPreferredSize(new Dimension(getWidth(), 19));
-		add(lblDestFloor);
+		GridBagLayout gridBagLayout = new GridBagLayout();
+		gridBagLayout.rowHeights = new int[] { 32, 32 };
+		gridBagLayout.columnWidths = new int[] { 32, 10 };
+		setLayout(gridBagLayout);
 		
 		// label with image
 		Image imgScaled = new ImageIcon(this.imgPath).getImage().getScaledInstance(32, 32, Image.SCALE_SMOOTH);
 		ImageIcon img = new ImageIcon(imgScaled);
+
 		JLabel lblImg = new JLabel(img);
-		lblImg.setPreferredSize(new Dimension(32, 32));
-		add(lblImg);
 		
+		GridBagConstraints gbc_lblImg = new GridBagConstraints();
+		gbc_lblImg.fill = GridBagConstraints.BOTH;
+		gbc_lblImg.gridx = 0;
+		gbc_lblImg.gridy = 0;
+		add(lblImg, gbc_lblImg);
+
+		// label for floor destination
+		JLabel lblDestFloor = new JLabel(Integer.toString(floorDest));
+		lblDestFloor.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		
+		GridBagConstraints gbc_lblDestFloor = new GridBagConstraints();
+		gbc_lblDestFloor.anchor = GridBagConstraints.FIRST_LINE_END;
+		gbc_lblDestFloor.gridx = 1;
+		gbc_lblDestFloor.gridy = 0;
+		add(lblDestFloor, gbc_lblDestFloor);
+
 	}
-		
+
 	/**
 	 * Update the NPC image
+	 * 
 	 * @param imgPath
 	 */
 	public void setImgPath(String imgPath) {
