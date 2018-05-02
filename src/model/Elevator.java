6c58f2f7e4d26c0a48b23f97b818fbf6c05c3220
Fábio Gaspar <fabiodanielreis@hotmail.com>
2018-05-02 10:42:23 +0100

    Added javadoc for Elevator

--- src/model/Elevator.java
+++ src/model/Elevator.java
@@ -1,24 +1,41 @@
 package model;
 
 public class Elevator extends NPCContainer {
-
+	
+	private int speed;
+	
+	/**
+	 * Constructor
+	 * @param cap This elevator capacity (maximum number of NPCs inside)
+	 * @param speed This elevator base speed
+	 */
 	public Elevator(int cap, int speed) {
 		this.setCapacity(cap);
 		this.setSpeed(speed);
 	}
-
-	private int speed;
-
+	
+	/**
+	 * @return The elevator base speed
+	 */
 	public int getSpeed() {
 		return speed;
 	}
 
+	/**
+	 * Updates the elevator base speed
+	 * @param speed
+	 */
 	public void setSpeed(int speed) {
 		if (speed <= 0 || speed > 5)
 			throw new IllegalArgumentException("speed must be between 1 and 5.");
 		this.speed = speed;
 	}
-
+	
+	/**
+	 * Adds a NPC inside the elevator and updates the NPC location
+	 * @see model.NPCContainer#addNPC(NPC)
+	 * @return True if the NPC was added, false otherwise
+	 */
 	public boolean addNPC(NPC n) {
 		if (super.addNPC(n)) {
 			n.setLocation(NPCLocation.LIFT);
@@ -26,7 +43,10 @@ public class Elevator extends NPCContainer {
 		}
 		return false;
 	}
-
+	
+	/**
+	 * Updates this elevator capacity (maximum number of NPCs inside)
+	 */
 	@Override
 	public void setCapacity(int cap) {
 		if (cap < 1)
