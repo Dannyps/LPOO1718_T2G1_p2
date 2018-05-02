df599ea7af69f4a1be89ffdd83f4db6a2f855483
Fábio Gaspar <fabiodanielreis@hotmail.com>
2018-05-02 10:48:20 +0100

    Javadoc for NPCContainer

--- src/model/NPCContainer.java
+++ src/model/NPCContainer.java
@@ -4,32 +4,66 @@ import java.util.ArrayList;
 import java.util.List;
 
 public abstract class NPCContainer {
-
+	/**
+	 * List of NPCs on this container
+	 */
 	List<NPC> npcs = new ArrayList<NPC>();
+	
+	/**
+	 * Maximum number of NPCs this container can hold
+	 */
 	int capacity;
 	
-	public int getCapacity() {
-		return capacity;
-	}
-
-	public abstract void setCapacity(int cap);
-
-	public List<NPC> getNpcs() {
-		return npcs;
-	}
-
+	/**
+	 * Constructor
+	 */
 	public NPCContainer() {
 		super();
 	}
-
+	
+	/**
+	 * 
+	 * @return The container capacity
+	 */
+	public int getCapacity() {
+		return capacity;
+	}
+	
+	/**
+	 * Updates the capacity
+	 * @param cap New capacity
+	 */
+	public abstract void setCapacity(int cap);
+	
+	/**
+	 *
+	 * @return List of NPCs inside the container
+	 */
+	public List<NPC> getNpcs() {
+		return npcs;
+	}
+	
+	/**
+	 * 
+	 * @return Number of NPCs inside the container
+	 */
 	public int getFillCount() {
 		return npcs.size();
 	}
 
+	/**
+	 * 
+	 * @return Number of free spots remaining
+	 */
 	public int getEmptyCount() {
 		return this.capacity - this.getFillCount();
 	}
-
+	
+	/**
+	 * Adds a NPC to the container
+	 * @param npc
+	 * @return True if it NPC was added successfully. False otherwise
+	 */
 	public boolean addNPC(NPC npc) {
 		if (this.getEmptyCount() < 1 || this.npcExists(npc)) {
 			return false;
@@ -38,7 +72,12 @@ public abstract class NPCContainer {
 			return true;
 		}
 	}
-
+	
+	/**
+	 * Check if some NPC is inside this container
+	 * @param npc
+	 * @return True if NPC exists inside the container. False otherwise
+	 */
 	private boolean npcExists(NPC npc) {
 		for (NPC n : this.npcs) {
 			if (n.equals(npc))
@@ -46,7 +85,12 @@ public abstract class NPCContainer {
 		}
 		return false;
 	}
-
+	
+	/**
+	 * Removes a NPC from the container
+	 * @param npc
+	 * @return True if the NPC was removed, false if the specified NPC doesn't exist on the container
+	 */
 	public boolean removeNPC(NPC npc) {
 		return npcs.remove(npc);
 	}
