ae46ea992f0f54bd554938d7d549a6863e5fe4cc
Daniel Silva <danny@dannyps.net>
2018-05-16 10:30:22 +0100

    Reorganize imports on welcome screen

--- src/view/WelcomeScreen.java
+++ src/view/WelcomeScreen.java
@@ -1,31 +1,23 @@
 package view;
 
 import java.awt.EventQueue;
-
-import javax.swing.JFrame;
-
-import controller.Controller;
-import javax.swing.JLabel;
-import java.awt.BorderLayout;
 import java.awt.Font;
-import javax.swing.JPanel;
-import java.awt.GridLayout;
-import java.awt.CardLayout;
-import javax.swing.JComboBox;
-import java.awt.GridBagLayout;
 import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.awt.Insets;
-import com.jgoodies.forms.layout.FormLayout;
-import com.jgoodies.forms.layout.ColumnSpec;
-import com.jgoodies.forms.layout.RowSpec;
-import javax.swing.DefaultComboBoxModel;
-import com.jgoodies.forms.layout.FormSpecs;
-import javax.swing.JTextField;
-import javax.swing.JButton;
-import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.DefaultComboBoxModel;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 import javax.swing.JSpinner;
 import javax.swing.SpinnerNumberModel;
+
+import controller.Controller;
 
 public class WelcomeScreen {
 
