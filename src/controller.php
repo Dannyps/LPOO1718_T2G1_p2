0313fc7e5c2a91f8e024fe2c061cc4a59b21b27a
Daniel Silva <danny@dannyps.net>
2018-05-16 14:42:00 +0100

    Fix highscore getter

--- src/controller.php
+++ src/controller.php
@@ -8,12 +8,13 @@ class Controller{
         $this->link = $link;
     }
 
-    public static function getHighScores($request, $g){
-        var_dump($g);
-        return 201;
+    public function getHighScores($request){
+        $res = mysqli_query($this->link, "SELECT * FROM scores ORDER BY score DESC LIMIT 15");
+        echo json_encode(mysqli_fetch_assoc($res));
+        return 200;
     }
 
     public static function postHighScores($request, $p){
-        var_dump($g);
+        var_dump($p);
     }
 }
