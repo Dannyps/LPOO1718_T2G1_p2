0e540d0ad579ef653b7eeb7271d491ddd8432db3
Daniel Silva <danny@dannyps.net>
2018-05-23 09:22:14 +0100

    Enable loading of highscores

--- src/view/HighScores.java
+++ src/view/HighScores.java
@@ -6,7 +6,17 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import java.awt.BorderLayout;
 import java.awt.Font;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.Scanner;
+
 import javax.swing.SwingConstants;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 import javax.swing.JPanel;
 import javax.swing.JTextPane;
 
@@ -35,15 +45,21 @@ public class HighScores {
 	 * Create the application.
 	 */
 	public HighScores() {
-		initialize();
+		try {
+			initialize();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 	/**
 	 * Initialize the contents of the frame.
+	 * @throws IOException 
 	 */
-	private void initialize() {
+	private void initialize() throws IOException {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
+		frame.setBounds(100, 100, 358, 394);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().add(panel, BorderLayout.NORTH);
 		
@@ -55,7 +71,33 @@ public class HighScores {
 		JTextPane textPane = new JTextPane();
 		frame.getContentPane().add(textPane, BorderLayout.CENTER);
 		
-		textPane.setText("teste");
+		textPane.setText(this.getHighScores());
+		
 	}
-
+	
+	private String getHighScores() throws IOException {
+		// build a URL
+	    String s = "http://lpoo.dannyps.net/highscores";
+	    URL url = new URL(s);
+	 
+	    // read from the URL
+	    Scanner scan = new Scanner(url.openStream());
+	    String str = new String();
+	    while (scan.hasNext())
+	        str += scan.nextLine();
+	    scan.close();
+	    
+	    System.out.println(str);
+	    JSONArray arr = new JSONArray(str);
+	    
+	    String out = "";
+	    for(Object obj : arr){
+		   JSONObject o = (JSONObject) obj;
+	    	out+= (o.get("name") + ": " + o.get("score") + "\n");
+	    }
+	    
+	    return out;
+	}
 }
+
+	
