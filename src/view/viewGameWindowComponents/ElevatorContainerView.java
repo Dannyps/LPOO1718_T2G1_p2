df2ab68af48c9f9231739b1a9f389936bde80d97
Fábio Gaspar <fabiodanielreis@hotmail.com>
2018-05-29 18:24:42 +0100

    Fixed line delimitations in ElevatorContainer

--- src/view/viewGameWindowComponents/ElevatorContainerView.java
+++ src/view/viewGameWindowComponents/ElevatorContainerView.java
@@ -22,9 +22,15 @@ public class ElevatorContainerView extends JPanel{
 	protected void paintComponent(Graphics g) {
 		super.paintComponent(g);
 		
-		for(int i = 0; i <= numberCells; i++) {
+		// draw vertical lines
+		g.drawLine(0, 0, 0, this.getHeight()-1);
+		g.drawLine(this.getWidth()-1, 0, this.getWidth()-1, this.getHeight()-1);
+		g.drawLine(0, 0, this.getWidth()-1, 0);
+		for(int i = 1; i <= numberCells; i++) {
 			int thisY = i * this.getHeight()/numberCells;
-			g.drawLine(0, thisY , thisY, this.getWidth());
+			g.drawLine(0, thisY-1, this.getWidth(), thisY-1);
 		}
+		
+		//g.drawLine(0, this.getHeight()-1, this.getWidth(), this.getHeight()-1);
 	}
 }
