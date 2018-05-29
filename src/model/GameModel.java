ea7e0b889effea51e411f9bd1250c73a7e65a6da
Fábio Gaspar <fabiodanielreis@hotmail.com>
2018-05-29 18:50:29 +0100

    Added GameModel

---
+++ src/model/GameModel.java
@@ -0,0 +1,14 @@
+package model;
+
+import java.util.List;
+
+import model.entities.*;
+
+public class GameModel {
+	/**
+	 * This is what represents the current playing game
+	 * It will have methods to get all buildings, all elevators, etc etc
+	 */
+	
+	private List<BuildingModel> buildings;
+}
